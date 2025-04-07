212691fef6feae20b3b20a7ebb4a4847e2207d76
Kumar <s.kumar@designpluz.com.au>
2025-04-07 21:30:23 +0530

    ...

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-        res.status(200).json({ message: 'Hello from Vercel Serverless Function!' });
-    } else {
-        res.status(405).json({ error: 'Method not allowed' });
-    }
-}
