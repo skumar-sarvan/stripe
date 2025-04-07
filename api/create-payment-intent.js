f1aa94fe91713efc657230b6f2e6099e9ca5a559
Kumar <s.kumar@designpluz.com.au>
2025-04-07 21:29:15 +0530

    test

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,25 +1,34 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res){
+// export default async function handler(req, res){
+//     if (req.method === 'GET') {
+//         const { amount } = req.query;
+
+//         const amountInt = parseInt(amount);
+//         if (isNaN(amountInt)) {
+//             return res.status(400).json({ error: 'Amount must be a number' });
+//         }
+
+//         try {
+//             // const paymentIntent = await stripe.paymentIntents.create({
+//             //     amount: amountInt,
+//             //     currency: 'aud',
+//             // });
+//             return res.status(200).json({ clientSecret: process.env.STRIPE_SECRET_KEY });
+//         } catch (error) {
+//             console.error('Stripe error:', error);
+//             return res.status(500).json({ error: error.message });
+//         }
+//     } else {
+//         return res.status(405).json({ error: 'Method not allowed' });
+//     }
+// }
+
+
+export default function handler(req, res) {
     if (req.method === 'GET') {
-        const { amount } = req.query;
-
-        const amountInt = parseInt(amount);
-        if (isNaN(amountInt)) {
-            return res.status(400).json({ error: 'Amount must be a number' });
-        }
-
-        try {
-            // const paymentIntent = await stripe.paymentIntents.create({
-            //     amount: amountInt,
-            //     currency: 'aud',
-            // });
-            return res.status(200).json({ clientSecret: process.env.STRIPE_SECRET_KEY });
-        } catch (error) {
-            console.error('Stripe error:', error);
-            return res.status(500).json({ error: error.message });
-        }
+        res.status(200).json({ message: 'Hello from Vercel Serverless Function!' });
     } else {
-        return res.status(405).json({ error: 'Method not allowed' });
+        res.status(405).json({ error: 'Method not allowed' });
     }
 }
