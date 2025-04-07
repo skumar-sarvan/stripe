7b20d39daf3ccb5415890010c957e45033961fa0
Kumar <s.kumar@designpluz.com.au>
2025-04-07 21:31:55 +0530

    ...

--- api/test.js
+++ api/test.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     if (req.method === 'GET') {
-        res.status(200).json({ message: 'Hello from Vercel Serverless Function!' });
+        res.status(200).json({ message: process.env.STRIPE_SECRET_KEY });
     } else {
         res.status(405).json({ error: 'Method not allowed' });
     }
