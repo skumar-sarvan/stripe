c084399e9a718d4f3081c48a6910b9e29fde3308
Kumar <s.kumar@designpluz.com.au>
2025-04-08 13:21:54 +0530

    ...

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -54,6 +54,8 @@ export default async function handler(req, res) {
         return res.status(200).json({ 
             status: true, 
             message: {
+                data: paymentIntent,
+                eKey: ephemeralKey,
                 clientSecret: paymentIntent.client_secret,
                 ephemeralKey: ephemeralKey.secret,
                 customerId: customerId
