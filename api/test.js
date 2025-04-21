bdc27057802b0b159bf251eaebc868a5288186d6
Kumar <s.kumar@designpluz.com.au>
2025-04-21 16:39:13 +0530

    ...

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-        res.status(200).json({ message: process.env.STRIPE_SECRET_KEY });
-    } else {
-        res.status(405).json({ error: 'Method not allowed' });
-    }
-}
